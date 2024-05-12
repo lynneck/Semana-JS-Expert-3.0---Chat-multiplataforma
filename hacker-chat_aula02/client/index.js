8292a8578aaf634547b7966d9efcb75613929167
lynneck <lynneckmartins@.com>
2024-05-12 00:02:02 -0300

    second commit

--- hacker-chat_aula02/client/index.js
+++ hacker-chat_aula02/client/index.js
@@ -7,14 +7,16 @@
 import Events from 'node:events'
 import TerminalController from "./src/terminalController.js";
 import CliConfig from './src/cliConfig.js';
+import SocketClient from './src/socket.js';
 
  const [nodePath, filePath, ...commands ] = process.argv
  const config = CliConfig.parseArguments(commands)
- console.log('config', config);
+ //console.log('config', config);
 
 //console.log(process.argv);
 const componentEmitter = new Events()
-
+const socketClient = new SocketClient(config)
+await socketClient.initialize()
 
 //const controller = new TerminalController()
 
--- hacker-chat_aula02/server/src/socket.js
+++ hacker-chat_aula02/server/src/socket.js
@@ -1,5 +1,8 @@
 
 import http from 'http'
+import { v4 } from 'uuid'
+import { constants} from './constants.js' 
+
 
 
 export default class SocketServer {
@@ -11,6 +14,19 @@ export default class SocketServer {
         const server = http.createServer((req, res) => {
             res.writeHead(200, {'Content-Type': 'test/plain'})
             res.end('hey there!!')
+        })
+
+        server.on('upgrade', (req, socket) => {
+            socket.id = v4()
+            const headers = [
+                'HTTP/1.1 101 Web Socket Protocol Handshake',
+                'Upgrade: WebSocket',
+                'Connection: Upgrade',
+                ''
+            ].map(line => line.concat('\r\n')).join('')
+
+            socket.write(headers)
+            eventEmitter.emit(constants.event.NEW_USER_CONNECTED, socket)
         })
 
         return new Promise((resolve, rejects) => {
